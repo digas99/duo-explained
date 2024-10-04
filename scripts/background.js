ea2b016676b39d8613d5068faff3f3d563882814
Diogo Correia <diogo.correia99@ua.pt>
2024-10-04 16:43:14 +0100

    added extension button to duolingo page

--- scripts/background.js
+++ scripts/background.js
@@ -1,21 +1,9 @@
 import { OpenAIAgent } from "./chatgpt.js";
 
-let injected = false;
-
-chrome.webNavigation.onDOMContentLoaded.addListener((details) => {
-  if (injected) return;
-  const tabId = details.tabId;
-  chrome.scripting.executeScript({
-    target: { tabId: tabId },
-    files: ["scripts/content.js"],
-  });
-  injected = true;
-});
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log(request);
   if (request.type === "QUERY") {
-    const agent = new OpenAIAgent();
-    agent.init();
+    // const agent = new OpenAIAgent();
+    // agent.init();
   }
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,26 @@
 chrome.runtime.sendMessage({ type: "QUERY" });
+
+window.onload = () => {
+	const moreNav = document.querySelector("a[data-test='home-nav']")?.parentElement;
+	const navbar = moreNav?.parentElement;
+	if (navbar) {
+		// clone the moreNav element
+		const newTab = makeTab(moreNav);
+
+		// add the new tab to the navbar
+		navbar.insertBefore(newTab, navbar.firstElementChild);
+	}
+}
+
+const makeTab = (template) => {
+	const tab = template.cloneNode(true);
+	tab.className = "d-cgpt-tab";
+	const tabLink = tab.querySelector("a");
+	delete tabLink.dataset.test;
+	tabLink.href = "javascript: void(0)";
+	const tabText = tab.querySelector("span span");
+	tabText.innerText = "Duo ChatGPT";
+	const tabIcon = tab.querySelector("img");
+	tabIcon.src = "https://andreclerigo.github.io/duolingo-chatgpt-assets/logo.png";
+	return tab;
+}
