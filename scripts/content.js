8a3d5bf8e02bbad1b44cc0366ab1c77cefa4bfae
Diogo Correia <diogo.correia99@ua.pt>
2024-10-04 17:56:36 +0100

    prompt to add api key

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,48 @@
 chrome.runtime.sendMessage({ type: "QUERY" });
 
 window.onload = () => {
+	// Duo ChatGPT tab
 	const moreNav = document.querySelector("a[data-test='home-nav']")?.parentElement;
 	const navbar = moreNav?.parentElement;
+	let newTab;
 	if (navbar) {
-		// clone the moreNav element
-		const newTab = makeTab(moreNav);
-
-		// add the new tab to the navbar
+		newTab = makeTab(moreNav);
 		navbar.insertBefore(newTab, navbar.firstElementChild);
 	}
+
+	// API Key prompt
+	chrome.storage.sync.get("API_KEY", data => {
+		const apiKey = data.API_KEY;
+		console.log(apiKey);
+		if (!apiKey) {
+			const prompt = makePrompt();
+			const root = document.querySelector("#root");
+			root.insertAdjacentHTML("beforeend", prompt);
+			const promptButton = document.querySelector(".d-cgpt-prompt button");
+			promptButton.addEventListener("click", () => {
+				const apiKey = document.querySelector(".d-cgpt-prompt input").value;
+				if (!apiKey) {
+					return;
+				}
+		
+				chrome.runtime.sendMessage({ type: "SET_API_KEY", apiKey: apiKey }, (response) => {
+					console.log(response);
+					if (response) {
+						document.querySelector(".d-cgpt-prompt").remove();
+						if (newTab) {
+							newTab.querySelector("a").classList.add("active");
+						}
+					}
+				});
+			});
+		}
+		else {
+			if (newTab) {
+				newTab.querySelector("a").classList.add("active");
+			}
+		}
+	});
+
 }
 
 const makeTab = (template) => {
@@ -24,3 +57,16 @@ const makeTab = (template) => {
 	tabIcon.src = "https://andreclerigo.github.io/duolingo-chatgpt-assets/logo.png";
 	return tab;
 }
+
+const makePrompt = () => {
+	return /* html */`
+		<div class="d-cgpt-prompt">
+			<div>
+				<img src="https://andreclerigo.github.io/duolingo-chatgpt-assets/logo.png">
+				<input type="text" placeholder="API Key">
+				<button>Submit</button>
+				<img src="https://andreclerigo.github.io/duolingo-chatgpt-assets/icons/close-thick.png">
+			</div>
+		</div>
+	`;
+};
